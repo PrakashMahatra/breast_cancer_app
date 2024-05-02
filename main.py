287102affde96b3dfd792f3326bb36bc1d30cd9a
Prakash Mahatra <97676406+PrakashMahatra@users.noreply.github.com>
2024-05-03 01:00:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import pickle as pickle
+def create_model(data):
+    X = data.drop(['diagnosis'], axis=1)
+    y = data['diagnosis']
+
+    # scaling and normalization
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # split the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # train the model
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    # test the model
+    y_pred = model.predict(X_test)
+    print('Accuracy of our model: ', accuracy_score(y_test, y_pred))
+    print('Classification report: \n', classification_report(y_test, y_pred))
+    return model, scaler
+
+
+
+
+def clean_data():
+    data = pd.read_csv("data.csv")
+
+    # data cleaning
+
+    data = data.drop(['Unnamed: 32','id'], axis=1)
+
+    # one hot encoding using map
+    data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
+
+    return data
+
+
+def main():
+    data = clean_data()
+
+    model, scaler = create_model(data)
+
+    with open('model.pkl','wb') as f:
+        pickle.dump(model, f)
+
+    with open('scaler.pkl','wb') as f:
+        pickle.dump(scaler,f)
+
+if __name__ == '__main__':
+    main()
